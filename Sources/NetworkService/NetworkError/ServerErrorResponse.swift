0023ba9008874a80a32b1235c3f24b034262c6e6
NadzeyaShpakouskaya <shpakovna@gmail.com>
2025-01-22 16:44:20 +0400

    rename error scheme

--- Sources/NetworkService/NetworkError/ServerErrorResponse.swift
+++ Sources/NetworkService/NetworkError/ServerErrorResponse.swift
@@ -2,5 +2,5 @@ import Foundation
 
 // Define a struct to match the expected error response from the backend
 struct ServerErrorResponse: Codable {
-    let error: String
+    let message: String
 }
